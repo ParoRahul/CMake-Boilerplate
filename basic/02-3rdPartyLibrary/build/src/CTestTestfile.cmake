246da10ce922cd7e0fb2e3b36c414e8414118fc9
ParoRahul <r.das699@gmail.com>
2020-03-25 21:27:56 +0530

    initial Commit

--- basic/02-3rdPartyLibrary/build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/cpp/CMake-Boilerplate/basic/02-3rdPartyLibrary/src
-# Build directory: D:/cpp/CMake-Boilerplate/basic/02-3rdPartyLibrary/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
